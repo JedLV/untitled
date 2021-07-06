eb54863c8dceb597559153b16afc93a47f5e2886
编程 <1660873558@qq.com>
2021-07-06 16:08:21 +0800

    第二次提交

---
+++ src/ludante.java
@@ -0,0 +1,9 @@
+public class ludante{
+
+    public static void main(String args[]){
+        System.out.println("姓名：吕但丁");//
+        //System.out.println();
+        System.out.println("性别：男");
+        System.out.println("住址：河南省");
+    }
+}
